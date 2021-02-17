ce23582831ec7b9bfb637318a56da3470f936374
geekbrains <9022368@mail.ru>
2021-02-18 00:47:39 +0500

    Задание 3

---
+++ Lesson 1/3.py
@@ -0,0 +1,9 @@
+x = range(1, 21)
+
+for i in x:
+    if i == 1:
+        print(i, 'процент')
+    elif 1 < i <= 4:
+        print(i, 'процента')
+    elif 5 <= i < 21:
+        print(i, 'процентов')
