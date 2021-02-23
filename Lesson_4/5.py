529472dfec06dda57838df19c73dc263e5168b10
geekbrains <9022368@mail.ru>
2021-02-23 18:51:31 +0500

    Задание 5

--- Lesson_4/5.py
+++ Lesson_4/5.py
@@ -5,19 +5,23 @@ import sys
 
 link = requests.get('http://www.cbr.ru/scripts/XML_daily.asp').text
 soup = BeautifulSoup(link, 'lxml')
-
+date = str(soup.find(id='')).split('"')
 usd = str(soup.find(id='R01235')).split('>')
 eur = str(soup.find(id='R01239')).split('>')
 
 
 def currency_rates(code):
-    if code.lower() == 'usd':
-        print(f'Доллар США = {usd[10][:5]} руб.')
+    if code.lower() == 'date':
+        print(f'\nДата {date[1]}\n')
+    elif code.lower() == 'usd':
+        print(f'Доллар США = {usd[10][:5]} руб., {date[1]}')
     elif code.lower() == 'eur':
-        print(f'Евро       = {eur[10][:5]} руб.')
+        print(f'Евро       = {eur[10][:5]} руб., {date[1]}')
     else:
         print('None')
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit(1)
     currency_rates(sys.argv[1])
