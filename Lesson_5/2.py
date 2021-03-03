1f5e0c1563482df004fa957f9653bb19e4ebb1f5
softicer-67 <9022368@mail.ru>
2021-03-04 00:42:48 +0500

    Задание 2

---
+++ Lesson_5/2.py
@@ -0,0 +1,15 @@
+def num_generator(n):
+    num = (num for num in range(1, n + 1, 2))
+    return num
+
+
+nums_gen = num_generator(15)
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
