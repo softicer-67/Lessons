de834d247b4dc3cea82cf8e56dd572cdf6df9c58
softicer-67 <9022368@mail.ru>
2021-03-04 03:39:47 +0500

    Задание 3

---
+++ Lesson_5/3.py
@@ -0,0 +1,17 @@
+tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена', 'Андрей', 'Геннадий']
+klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
+
+new_gen = ((tutors[i], klasses[i]) for j in range(len(klasses)) for i in range(len(tutors)))
+if klasses < tutors:
+    klasses.append(None)
+
+print(next(new_gen))
+print(next(new_gen))
+print(next(new_gen))
+print(next(new_gen))
+print(next(new_gen))
+print(next(new_gen))
+print(next(new_gen))
+print(next(new_gen))
+print(next(new_gen))
+print(type(new_gen))
