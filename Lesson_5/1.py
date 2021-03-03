22a216023cbc60136ba54f5a03749d1940bc465c
softicer-67 <9022368@mail.ru>
2021-03-04 00:36:10 +0500

    Задание 1

---
+++ Lesson_5/1.py
@@ -0,0 +1,15 @@
+def num_generator(n):
+    for num in range(1, n + 1, 2):
+        yield num
+
+
+nums_gen = num_generator(15)
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
