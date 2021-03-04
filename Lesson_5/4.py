abd49d04aeb40fcaa848ac85a8a4f854dc63b87b
softicer-67 <9022368@mail.ru>
2021-03-04 12:42:49 +0500

    Задание 4

--- Lesson_5/4.py
+++ Lesson_5/4.py
@@ -2,8 +2,16 @@ src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
 res = []
 
-for i in range(len(src)-1):
-    if src[i] < src[i+1]:
-        res.append(src[i+1])
+for i in range(len(src) - 1):
+    if src[i] < src[i + 1]:
+        res.append(src[i + 1])
 
 print(res)
+
+
+result = [
+    src[i + 1] for i in range(len(src) - 1)
+    if src[i] < src[i + 1]
+]
+
+print(result)
