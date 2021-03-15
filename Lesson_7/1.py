8a3eca42a4896caf116ec79ef1400cce4ea7ac54
softicer-67 <9022368@mail.ru>
2021-03-16 01:07:41 +0500

    Задание 1

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -1,9 +1,13 @@
 import os
 
-try:
-    dir_name = '--my_project'
-    folders = ['--settings', '--mainapp', '--adminapp', '--authapp']
-    for folder in folders:
-        os.makedirs(os.path.join(dir_name, folder))
-except FileExistsError:
-    print('\nТакая папка уже существует!')
+path = 'my_project'
+folders = ['settings', 'mainapp', 'adminapp', 'authapp']
+
+for folder in folders:
+    try:
+        if not os.path.exists(folder):
+            os.makedirs(os.path.join(path, folder))
+            print(f'Добавлена папка: \\{folder}\\')
+    except FileExistsError:
+        print('')
+
