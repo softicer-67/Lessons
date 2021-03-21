d24aca2eb9e4543865b93c34282e65a7dbdbc3c5
softicer-67 <9022368@mail.ru>
2021-03-21 08:47:57 +0500

    Задание 2

---
+++ Lesson_10/2.py
@@ -0,0 +1,41 @@
+class Textile:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square_c(self):
+        return self.width / 6.5 + 0.5
+
+    def get_square_j(self):
+        return self.height * 2 + 0.3
+
+    @property
+    def get_sq_full(self):
+        return str(f'Общий расход ткани {(self.width / 6.5 + 0.5) + (2 * self.height + 0.3)}')
+
+
+class Coat(Textile):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.square_c = round(self.width / 6.5 + 0.5)
+
+    def __str__(self):
+        return f'Расход ткани на пальто {self.square_c}'
+
+
+class Jacket(Textile):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.square_j = round(self.height * 2 + 0.3)
+
+    def __str__(self):
+        return f'Расход ткани на костюм {self.square_j}'
+
+
+coat = Coat(3, 3)
+jacket = Jacket(2, 3)
+print(coat)
+print(jacket)
+print(coat.get_sq_full)
+print(jacket.get_sq_full)
+
