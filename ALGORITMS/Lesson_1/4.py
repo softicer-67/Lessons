94e676f69ae6c05913e1fd314229deb372469523
Yuri_1967 <9022368@mail.ru>
2021-07-12 13:26:12 +0500

    Задача 4

--- ALGORITMS/Lesson_1/4.py
+++ ALGORITMS/Lesson_1/4.py
@@ -7,3 +7,35 @@
 Например, если надо получить случайный символ от 'a' до 'f', то вводятся эти символы.
 Программа должна вывести на экран любой символ алфавита от 'a' до 'f' включительно.
 """
+
+import random
+import sys
+
+
+def menu():
+    print('\n\tМЕНЮ:\n')
+    print('[1] Вывести случайное целое число')
+    print('[2] Случайное вещественное число')
+    print('[3] Случайный символ')
+    print('[0] Выход')
+
+
+menu()
+option = input()
+
+while option != '0':
+    if option == '1':
+        print(random.randint(1, 100))
+    elif option == '2':
+        print(float(random.randrange(101)))
+    elif option == '3':
+        print(random.choice('abcdef'))
+
+    if __name__ == '__main__':
+        option = input()
+
+
+
+
+
+
