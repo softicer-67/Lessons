74cf40a1d452740d6bb0bbfdc628a4cc73836a35
Yuri_1967 <9022368@mail.ru>
2021-07-12 14:22:16 +0500

    Задача 5

--- ALGORITMS/Lesson_1/5.py
+++ ALGORITMS/Lesson_1/5.py
@@ -10,11 +10,3 @@
 
 number = input('Введите число: ')
 
-sum = 0
-prod = 1
-
-for f in number:
-    sum += int(f)
-    prod *= int(f)
-print(f"Сумма цифр числа {number}: {sum}")
-print(f"Произведение цифр: {number}: {prod}")
