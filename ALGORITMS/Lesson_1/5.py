f83526b343dd63cf5f26bf3137064ee128855fae
Yuri_1967 <9022368@mail.ru>
2021-07-12 14:19:41 +0500

    Задача 1

--- ALGORITMS/Lesson_1/5.py
+++ ALGORITMS/Lesson_1/5.py
@@ -1,3 +1,20 @@
 """
 5. Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят и сколько между ними находится букв.
 """
+
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#
+# a, b = input('Введите 2 буквы латинского алфавита: ')
+#
+# x = len(alphabet)
+
+number = input('Введите число: ')
+
+sum = 0
+prod = 1
+
+for f in number:
+    sum += int(f)
+    prod *= int(f)
+print(f"Сумма цифр числа {number}: {sum}")
+print(f"Произведение цифр: {number}: {prod}")
