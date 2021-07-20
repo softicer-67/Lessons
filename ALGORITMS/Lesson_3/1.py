728b9b3966b69c0b0eee24cd7d72b6d9e33524b6
Yuri_1967 <9022368@mail.ru>
2021-07-21 03:00:26 +0500

    Задача 1

--- ALGORITMS/Lesson_3/1.py
+++ ALGORITMS/Lesson_3/1.py
@@ -8,4 +8,4 @@ for i in range(2, 10):
     for j in range(2, 100):
         if j % i == 0:
             res[i].append(j)
-    print(f'Числу {i} кратны {len(res[i])} чисел: {res[i]}.')
+    print(f'Числу {i} кратны {len(res[i])} чисел: {res[i]}')
